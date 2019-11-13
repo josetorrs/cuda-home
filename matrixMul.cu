d2e3e8e500c2e65b519bb9738ab364f6c1a64d53
Jose Torres <josetorrs7@gmail.com>
2019-11-13 12:04:27 -0600

    empty

--- matrixMul.cu
+++ matrixMul.cu
@@ -10,40 +10,43 @@ __global__ void matrixMulCUDA(float *A, float *B, float *C, int size){
     __shared__ float smem_a[64][8];
     __shared__ float smem_b[8][64];
 
-    int col = blockIdx.x * 64;
-    int row = blockIdx.y * 64;
-    float val = 0;
-
-    int start = threadIdx.x + blockDim.x * threadIdx.y;
-    int stride = blockDim.x * blockDim.y;
-    int end = 64 * 8;
+    int c = blockIdx.x * 64;
+    int r = blockIdx.y * 64;
 
     for(int kk = 0; kk < size; kk += 8){
-        for(int i = start; i < end; i += stride){
+        for(int i = threadIdx.x + blockDim.x * threadIdx.y;
+                i < 64 * 8;
+                i += blockDim.x * blockDim.y){
             
+            // load into shared memory
             int k = kk + i / 64;
-            int rt = row + i % 64;
-            int ct = col + i % 64;
+            int rt = r + i % 64;
+            int ct = c + i % 64;
             smem_a[i%64][i/64] = A[rt*size+k];
             smem_b[i/64][i%64] = B[k*size+ct];
         
         }
         __syncthreads();
-        // ....
-        for(int k=kk; k < kk + 8; k++){
-            // val += smem_a[threadIdx.x][k] * smem_b[k][threadIdx.y];
-            smem_c[row][col] += smem_a[threadIdx.x][k] * smem_b[k][threadIdx.y];
+        for(int i=0; i < 2; ++i){
+            for(int j=0; j < 2; ++j){
+
+                int rowIdx = threadIdx.y * 2 + j;
+                int colIdx = threadIdx.x * 2 + i;
+
+                for(int k=0; k < 8; ++k){
+                    // Store / Compute results in shared C
+                    smem_c[rowIdx][colIdx] += smem_a[rowIdx][k] * smem_b[k][colIdx];
+                }
+                // Store back into global memory
+                C[(r+rowIdx) * size + (c+colIdx)] = smem_c[rowIdx][colIdx];
+            }
         }
-        __syncthreads();
     }
-    // int i = col + threadIdx.x;
-    // int j = row + threadIdx.y;
-    // C[i*size+j] = val;
 }
 
 int main() {
     
-    const int N = 8192; // something easily divisible by 256 (8*64)
+    const int N = 8192; 
 
     // Declare host memory for Matrices A and B
     float *hostA, *hostB, *hostC, *hostSumTemp;
@@ -53,10 +56,10 @@ int main() {
 
     // Allocate host memory for all Matrices
     size_t memSize = sizeof(float) * N * N;
-    hostA = reinterpret_cast<float* > (malloc(memSize));
-    hostB = reinterpret_cast<float* > (malloc(memSize));
-    hostC = reinterpret_cast<float* > (malloc(memSize));
-    hostSumTemp = reinterpret_cast<float* > (malloc(memSize));
+    hostA = (float*) malloc(memSize);
+    hostB = (float*) malloc(memSize);
+    hostC = (float*) malloc(memSize);
+    hostSumTemp = (float*) malloc(memSize);
     
     // init host memory
 
@@ -71,14 +74,14 @@ int main() {
             hostA[i*N+j] = 2;
             hostB[i*N+j] = 3;
             hostC[i*N+j] = 0;
-            hostSumTemp[i*N*j] = 6;
+            hostSumTemp[i*N+j] = 6;
         }
     }
 
     // Allocate device memory
-    cudaMalloc(reinterpret_cast<void **>(&deviceA), memSize);
-    cudaMalloc(reinterpret_cast<void **>(&deviceB), memSize);
-    cudaMalloc(reinterpret_cast<void **>(&deviceC), memSize);
+    cudaMalloc((void**) &deviceA, memSize);
+    cudaMalloc((void**) &deviceB, memSize);
+    cudaMalloc((void**) &deviceC, memSize);
 
     // Copy Matrix A and B from host to device
     cudaMemcpy(deviceA, hostA, memSize, cudaMemcpyHostToDevice);
@@ -89,17 +92,17 @@ int main() {
     dim3 nthreads(32, 32);
 
     // Init Kernel
-    matrixMulCUDA<<< nblocks, nthreads>>> (hostA, hostB, hostC, N);
+    matrixMulCUDA<<<nblocks, nthreads>>> (deviceA, deviceB, deviceC, N);
     
     // Transfer from result matrix from device to host
     cudaMemcpy(hostC, deviceC, memSize, cudaMemcpyDeviceToHost);
 
     // Check results
     bool isSame = true;
-    int in, out;
-    for(in=0; in < N; ++in){
-        for(out=0; out < N; ++out){
-            if(hostC[in*N*out] != hostSumTemp[in*N*out]){
+    int row, col;
+    for(row=0; row < N; ++row){
+        for(col=0; col < N; ++col){
+            if(hostC[row*N+col] != hostSumTemp[row*N+col]){
                 isSame = false;
                 break;
             }
@@ -108,7 +111,7 @@ int main() {
 
     // Print comparasion
     if(!isSame){
-        std::cout << "Did not match at \n\ti = " << in << "\n\tj = " << out << std::endl;
+        std::cout << "Did not match at \n\trow = " << row << "\n\tcol = " << col << std::endl;
     } else {
         std::cout << "Results matched.\n";
     }
